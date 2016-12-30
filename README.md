96ad5cd9989b983d165bb6b2b565a15c0a8a316a
Louis Pilfold <louis@lpil.uk>
2016-12-30 21:30:07 +0000

    2.4.3

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Exploring Matt Pearson's "Generative Art" book, with Elm!
 - [2.3.1: The frame loop](https://github.com/lpil/generative-elm/tree/master/arts/2.3.1)
 - [2.4.1: (A lack of) while loops](https://github.com/lpil/generative-elm/tree/master/arts/2.4.1)
 - [2.4.2: Leaving traces](https://github.com/lpil/generative-elm/tree/master/arts/2.4.2)
+- [2.4.3: (A lack of) for loops](https://github.com/lpil/generative-elm/tree/master/arts/2.4.3)
 
 
 ## Usage
---
+++ arts/2.4.3/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (..)
+
+import Html exposing (Html)
+import Collage exposing (..)
+import Element
+import Color as Color exposing (Color)
+import List as List
+
+
+lineStyle : Color -> LineStyle
+lineStyle color =
+    { defaultLine
+        | width = 5
+        , color = color
+    }
+
+
+background : Form
+background =
+    rect 500 300
+        |> filled (Color.rgb 180 180 180)
+
+
+fadingBlack : Int -> Color
+fadingBlack x =
+    Color.rgba 0 0 0 ((toFloat (255 - (x * 10))) / 255)
+
+
+growingWhite : Int -> Color
+growingWhite x =
+    Color.rgba 255 255 255 ((toFloat (x * 10)) / 255)
+
+
+lines : Int -> (Int -> Color) -> Form
+lines numLines makeColor =
+    List.range 0 numLines
+        |> List.map
+            (\x ->
+                segment ( 240, 0 ) ( -240, 0 )
+                    |> traced (makeColor x |> lineStyle)
+                    |> moveY (toFloat (140 - x * 10))
+            )
+        |> group
+
+
+main : Html msg
+main =
+    [ background
+    , lines 28 fadingBlack |> moveY 0
+    , lines 27 growingWhite |> moveY -5
+    ]
+        |> collage 500 300
+        |> Element.toHtml
